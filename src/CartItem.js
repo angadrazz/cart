f65c572caff759c26ef2479415edbeeee9381009
angadrazz <angadraj9534@gmail.com>
2023-03-24 18:00:37 +0530

    submit

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,61 +1,45 @@
-/* eslint-disable jsx-a11y/alt-text */
-
-import React from "react";
+import React from 'react';
 
 class CartItem extends React.Component {
-        constructor (){
-                super();
-                this.state = {
-                        price: 999,
-                        title: 'phone',
-                        qty: 1,
-                        img: ''
-                }
-                //this.increaseQuantity = this.increaseQuantity.bind(this);
-        }
-        increaseQuantity =  () => {
-                //console.log('this', this.state);
-                this.setState({
-                  qty: this.state.qty + 1
-                });
-        }
-        decreaseQuantity = () => {
-          this.setState({
-            qty: this.state.qty - 1
-          })
-        }
-  render() {
-        
-        const { price, title, qty } = this.state;
+  render () {
+    console.log('this.props', this.props);
+    const { price, title, qty } = this.props.product;
+    const {
+      product,
+      onIncreaseQuantity,
+      onDecreaseQuantity,
+      onDeleteProduct
+    } = this.props;
     return (
       <div className="cart-item">
+        {this.props.jsx}
         <div className="left-block">
-          <img style={styles.Image} />
+          <img src='' alt='' style={styles.image} />
         </div>
         <div className="right-block">
-          <div style={{ fontSize: 25, color: "blue" }}>{title}</div>
-          <div style={{ color: "#777" }}>Rs {price}</div>
-          <div style={{ color: "#777" }}>Qty: {qty}</div>
-          <div className="cart-item-actions left-block">
-            {/* Button */}
+          <div style={ { fontSize: 25 } }>{title}</div>
+          <div style={ { color: '#777' } }>Rs {price} </div>
+          <div style={ { color: '#777' } }>Qty: {qty} </div>
+          <div className="cart-item-actions">
+            {/* Buttons */}
             <img
               alt="increase"
               className="action-icons"
-              src="https://as1.ftcdn.net/v2/jpg/03/30/25/74/1000_F_330257485_WEd8LDmLkdp4vbJjhbLY263gV0OvsiIF.jpg"
-              onClick={this.increaseQuantity}
+              src="https://image.flaticon.com/icons/svg/992/992651.svg"
+              onClick={() => onIncreaseQuantity(product)}
             />
             <img
               alt="decrease"
               className="action-icons"
-              src="https://as2.ftcdn.net/v2/jpg/02/44/39/19/1000_F_244391909_DfS5TL9Oyzhf4VW0v9gPq4FPDg3BtD9w.jpg"
-              onClick={this.decreaseQuantity}
+              src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
+              onClick={() => onDecreaseQuantity(product)}
             />
             <img
               alt="delete"
               className="action-icons"
-              src="https://as2.ftcdn.net/v2/jpg/01/90/89/15/1000_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg"
+              src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+              onClick={() => onDeleteProduct(product.id)}
             />
-            
           </div>
         </div>
       </div>
@@ -64,12 +48,12 @@ class CartItem extends React.Component {
 }
 
 const styles = {
-  Image: {
+  image: {
     height: 110,
     width: 110,
     borderRadius: 4,
-    background: "#ccc",
-  },
-};
+    background: '#ccc'
+  }
+}
 
 export default CartItem;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Cart from './Cart';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-    <Cart />
-  </React.StrictMode>
-);
-
-
+ReactDOM.render(<App />, document.getElementById('root'));
