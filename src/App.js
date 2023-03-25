df7dc8cbfc31501df2b13caf0e052fddd18400c4
EasytolearnWebDeveloper <104381870+EasytolearnWebDeveloper@users.noreply.github.com>
2023-03-25 19:30:48 +0530

    yes change

--- src/App.js
+++ src/App.js
@@ -78,17 +78,14 @@ class App extends React.Component() {
   render (){
   return (
     <div className="App">
-<<<<<<< HEAD
       <Cart 
       onIncreaseQuantity={this.handleIncreaseQuantity}
       onDecreaseQuantity={this.handleDecreaseQuantity}
       onDeleteProduct={this.handleDeleteProduct}
       />
-=======
+
       <Navbar />
       <Cart />
-      
->>>>>>> 25e74a3cfd8a14d29629616c6d09f37807534d84
     </div>
   );
   }
