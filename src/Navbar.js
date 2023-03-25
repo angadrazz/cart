b1caacebb41af790e613f9a3409390c4385bb990
EasytolearnWebDeveloper <104381870+EasytolearnWebDeveloper@users.noreply.github.com>
2023-03-25 20:29:23 +0530

    count

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Navbar = () =>  {
+const Navbar = (props) =>  {
        return (
          <div style={styles.nav}>
          <div style={styles.cartIconContainer}>
            <img style={styles.cartIcon} src="https://cdn-icons-png.flaticon.com/512/3144/3144486.png" alt="" />
-           <span style={styles.cartCount}>3</span>
+           <span style={styles.cartCount}> {props.count} </span>
          </div>
          </div>
        )
