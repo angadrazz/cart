bb94db3085aabf3801b0b1af82553acb6ffb79ff
EasytolearnWebDeveloper <104381870+EasytolearnWebDeveloper@users.noreply.github.com>
2023-03-25 20:01:28 +0530

    render removed

--- src/Cart.js
+++ src/Cart.js
@@ -2,25 +2,23 @@ import React from 'react';
 import CartItem from './CartItem';
 
 const Cart = (props) => {
-  render () {
-    const { products } = props;
-    return (
-      <div className="cart">
-        {products.map((product) => {
-          return (
-            <CartItem
-              product={product}
-              key={product.id}
-              onIncreaseQuantity={props.onIncreaseQuantity}
-              onDecreaseQuantity={props.onDecreaseQuantity}
-              onDeleteProduct={props.onDeleteProduct}
-            />
-        
-          )
-        })}
-      </div>
-    );
-  }
+  const { products } = props;
+  return (
+    <div className="cart">
+      {products.map((product) => {
+        return (
+          <CartItem
+            product={product}
+            key={product.id}
+            onIncreaseQuantity={props.onIncreaseQuantity}
+            onDecreaseQuantity={props.onDecreaseQuantity}
+            onDeleteProduct={props.onDeleteProduct}
+          />
+
+        )
+      })}
+    </div>
+  );
 }
 
 export default Cart;
